cbb13b525cf6c3ca7c634348c09d9c5c21dd13f9
bkekelic <berky9@gmail.com>
2018-11-05 15:02:17 +0100

    Prva verzija programa

---
+++ SemaforZaPjesake/SemaforZaPjesake/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SemaforZaPjesake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pb_Scene = new System.Windows.Forms.PictureBox();
+            this.btnStartScene = new System.Windows.Forms.Button();
+            this.timerForAll = new System.Windows.Forms.Timer(this.components);
+            this.btn_TurnONPedGreen = new System.Windows.Forms.Button();
+            this.lb_PedWait = new System.Windows.Forms.Label();
+            this.timerStopage = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Scene)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_Scene
+            // 
+            this.pb_Scene.BackColor = System.Drawing.Color.White;
+            this.pb_Scene.Location = new System.Drawing.Point(67, 24);
+            this.pb_Scene.Name = "pb_Scene";
+            this.pb_Scene.Size = new System.Drawing.Size(1044, 537);
+            this.pb_Scene.TabIndex = 0;
+            this.pb_Scene.TabStop = false;
+            // 
+            // btnStartScene
+            // 
+            this.btnStartScene.Location = new System.Drawing.Point(1140, 24);
+            this.btnStartScene.Name = "btnStartScene";
+            this.btnStartScene.Size = new System.Drawing.Size(128, 59);
+            this.btnStartScene.TabIndex = 1;
+            this.btnStartScene.Text = "Turn ON traffic lights";
+            this.btnStartScene.UseVisualStyleBackColor = true;
+            this.btnStartScene.Click += new System.EventHandler(this.btnStartScene_Click);
+            // 
+            // timerForAll
+            // 
+            this.timerForAll.Interval = 1000;
+            this.timerForAll.Tick += new System.EventHandler(this.timerForAll_Tick);
+            // 
+            // btn_TurnONPedGreen
+            // 
+            this.btn_TurnONPedGreen.Location = new System.Drawing.Point(1140, 89);
+            this.btn_TurnONPedGreen.Name = "btn_TurnONPedGreen";
+            this.btn_TurnONPedGreen.Size = new System.Drawing.Size(128, 70);
+            this.btn_TurnONPedGreen.TabIndex = 2;
+            this.btn_TurnONPedGreen.Text = "Turn ON pedestrian green light";
+            this.btn_TurnONPedGreen.UseVisualStyleBackColor = true;
+            this.btn_TurnONPedGreen.Click += new System.EventHandler(this.btn_TurnONPedGreen_Click);
+            // 
+            // lb_PedWait
+            // 
+            this.lb_PedWait.AutoSize = true;
+            this.lb_PedWait.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_PedWait.ForeColor = System.Drawing.Color.Red;
+            this.lb_PedWait.Location = new System.Drawing.Point(1202, 183);
+            this.lb_PedWait.Name = "lb_PedWait";
+            this.lb_PedWait.Size = new System.Drawing.Size(0, 25);
+            this.lb_PedWait.TabIndex = 3;
+            // 
+            // timerStopage
+            // 
+            this.timerStopage.Interval = 1000;
+            this.timerStopage.Tick += new System.EventHandler(this.timerStopage_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 593);
+            this.Controls.Add(this.lb_PedWait);
+            this.Controls.Add(this.btn_TurnONPedGreen);
+            this.Controls.Add(this.btnStartScene);
+            this.Controls.Add(this.pb_Scene);
+            this.Name = "Form1";
+            this.Text = "Semafor za pješake";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Scene)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_Scene;
+        private System.Windows.Forms.Button btnStartScene;
+        private System.Windows.Forms.Timer timerForAll;
+        private System.Windows.Forms.Button btn_TurnONPedGreen;
+        private System.Windows.Forms.Label lb_PedWait;
+        private System.Windows.Forms.Timer timerStopage;
+    }
+}
+
---
+++ SemaforZaPjesake/SemaforZaPjesake/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemaforZaPjesake
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics graph;
+        bool stateCounter = false;
+        int timeBlinkCounter = 0;
+        string mode = "";
+        int timeCounter;
+        bool pressPedButton = false;
+        bool sceneIsOn = false;
+
+        List<SolidBrush> brushList = new List<SolidBrush>();
+        List<Point> pointLightsList = new List<Point>();
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pb_Scene.Size.Width, pb_Scene.Size.Height);
+            graph = Graphics.FromImage(bmp);
+            timeCounter = 0;
+
+
+            AddPointsToList();
+            AddBrushesToList();
+            ShowTrafficLights();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerForAll.Interval = 500;
+            mode = "OrangeBlinkAllTheTime";
+            TrafficLightsChange();
+        }
+        private void AddPointsToList()
+        {
+            //car lights
+            pointLightsList.Add(new Point(800, 20));
+            pointLightsList.Add(new Point(800, 60));
+            pointLightsList.Add(new Point(800, 100));
+            pointLightsList.Add(new Point(200, 390));
+            pointLightsList.Add(new Point(200, 430));
+            pointLightsList.Add(new Point(200, 470));
+
+            //pedestrial lights
+            pointLightsList.Add(new Point(700, 20));//crveno 6
+            pointLightsList.Add(new Point(700, 60));//zeleno 7
+            pointLightsList.Add(new Point(300, 390));//crveno 8 
+            pointLightsList.Add(new Point(300, 430));//zeleno 9
+
+
+        }
+        private void AddBrushesToList()
+        {
+            brushList.Add(new SolidBrush(Color.Black));
+            brushList.Add(new SolidBrush(Color.Black));
+            brushList.Add(new SolidBrush(Color.Black));
+            brushList.Add(new SolidBrush(Color.Red));
+            brushList.Add(new SolidBrush(Color.Orange));
+            brushList.Add(new SolidBrush(Color.FromArgb(0,192,0)));
+        }
+        private void ShowTrafficLights()
+        {
+            TurnOFFCarRed();
+            TurnOFFCarOrange();
+            TurnOFFCarGreen();
+
+            TurnOFFPedestrianRed();
+            TurnOFFPedestrianGreen();
+        }
+
+        private void DrawCircle(SolidBrush b, Point p)
+        {
+            graph = Graphics.FromImage(bmp);
+            graph.FillEllipse(b, p.X, p.Y, 35, 35);
+
+            RefreshScene();
+        }
+        private void RefreshScene()
+        {
+            pb_Scene.Image = bmp;
+            graph.Dispose();
+        }
+
+       
+
+        private void TurnOFFCarRed()
+        {
+            DrawCircle(brushList[0], pointLightsList[0]);
+            DrawCircle(brushList[0], pointLightsList[3]);
+
+        }
+        private void TurnOFFCarOrange()
+        {
+            DrawCircle(brushList[1], pointLightsList[1]);
+            DrawCircle(brushList[1], pointLightsList[4]);
+        }
+        private void TurnOFFCarGreen()
+        {
+            DrawCircle(brushList[2], pointLightsList[2]);
+            DrawCircle(brushList[2], pointLightsList[5]);
+        }
+        private void TurnONCarRed()
+        {
+            DrawCircle(brushList[3], pointLightsList[0]);
+            DrawCircle(brushList[3], pointLightsList[3]);
+        }
+        private void TurnONCarOrange()
+        {
+            DrawCircle(brushList[4], pointLightsList[1]);
+            DrawCircle(brushList[4], pointLightsList[4]);
+        }
+        private void TurnONCarGreen()
+        {
+            DrawCircle(brushList[5], pointLightsList[2]);
+            DrawCircle(brushList[5], pointLightsList[5]);
+        }
+
+        private void TurnOFFPedestrianRed()
+        {
+            DrawCircle(brushList[0], pointLightsList[6]);
+            DrawCircle(brushList[0], pointLightsList[8]);
+        }
+        private void TurnOFFPedestrianGreen()
+        {
+            DrawCircle(brushList[2], pointLightsList[7]);
+            DrawCircle(brushList[2], pointLightsList[9]);
+        }
+        private void TurnONPedestrianRed()
+        {
+            DrawCircle(brushList[3], pointLightsList[6]);
+            DrawCircle(brushList[3], pointLightsList[8]);
+        }
+        private void TurnONPedestrianGreen()
+        {
+            DrawCircle(brushList[5], pointLightsList[7]);
+            DrawCircle(brushList[5], pointLightsList[9]);
+        }
+
+
+        private void TrafficLightsChange()
+        {
+            timerForAll.Stop();
+
+            if (mode == "CarGreen")
+            {
+                TurnONCarGreen();
+                timerForAll.Interval = 20000;
+                mode = "CarGreenBlink";
+                stateCounter = false;
+
+                //ped
+                TurnONPedestrianRed();
+
+                timeCounter = 0;
+                timerStopage.Start();
+
+            }
+            else if (mode == "CarGreenBlink")
+            {
+                //treba upaliti zeleno
+                if (stateCounter == true)
+                {
+                    TurnONCarGreen();
+                    stateCounter = false;
+                }
+                //treba ugasiti zeleno
+                else
+                {
+                    TurnOFFCarGreen();
+                    stateCounter = true;
+                }
+                timeBlinkCounter++;
+                timerForAll.Interval = 500;
+
+                if (timeBlinkCounter == 7)
+                {                
+                    timeBlinkCounter = 0;
+                    TurnOFFCarGreen();
+                    TurnONCarOrange();
+                    mode = "AfterCarOrange";
+                    timerForAll.Interval = 2000;
+                    stateCounter = false;
+                }
+            }
+            else if (mode == "AfterCarOrange")
+            {
+                TurnOFFCarOrange();
+                TurnONCarRed();
+                mode = "TurnOnPedGreen";
+                timerForAll.Interval = 2000;
+            }
+            else if (mode == "TurnOnPedGreen")
+            {
+                TurnOFFPedestrianRed();
+                TurnONPedestrianGreen();
+                mode = "PedGreenBlink";
+                timerForAll.Interval = 8000;
+            }
+            else if (mode == "PedGreenBlink")
+            {
+                //treba upaliti zeleno
+                if (stateCounter == true)
+                {
+                    TurnONPedestrianGreen();
+                    stateCounter = false;
+                }
+                //treba ugasiti zeleno
+                else
+                {
+                    TurnOFFPedestrianGreen();
+                    stateCounter = true;
+                }
+                timeBlinkCounter++;
+                timerForAll.Interval = 500;
+
+                if (timeBlinkCounter == 7)
+                {
+                    timeBlinkCounter = 0;
+                    TurnONPedestrianRed();
+                    mode = "AfterCarRed";
+                    timerForAll.Interval = 3000;
+                    stateCounter = false;
+                }
+
+            }
+            else if (mode == "AfterCarRed")
+            {
+                TurnONCarOrange();
+                mode = "AfterCarRedOrangeToGreen";
+                timerForAll.Interval = 2000;
+            }
+            else if (mode == "AfterCarRedOrangeToGreen")
+            {
+                TurnOFFCarRed();
+                TurnOFFCarOrange();
+                TurnONCarGreen();
+
+                mode = "CarGreenBlink";
+                timerForAll.Interval = 20000;
+
+                timeCounter = 0;
+                timerStopage.Start();
+
+            }
+            else if (mode == "OrangeBlinkAllTheTime")
+            {
+                timerForAll.Interval = 500;
+
+                //treba upaliti narandasto
+                if (stateCounter == true)
+                {
+                    TurnONCarOrange();
+                    stateCounter = false;
+                }
+                //treba ugasiti narandasto
+                else
+                {
+                    TurnOFFCarOrange();
+                    stateCounter = true;
+                }
+  
+            }
+            
+            timerForAll.Start();
+        }
+        
+        private void timerForAll_Tick(object sender, EventArgs e)
+        {
+            TrafficLightsChange();
+        }
+
+        private void btnStartScene_Click(object sender, EventArgs e)
+        {
+            if (sceneIsOn == false)
+            {
+                mode = "AfterCarRed";
+                TurnONCarRed();
+                TurnONPedestrianRed();
+                TurnOFFCarOrange();
+
+                timerForAll.Interval = 3000;
+                timerForAll.Start();
+                sceneIsOn = true;
+                btnStartScene.Text = "Turn OFF traffic lights";
+            }
+            else if(sceneIsOn == true)
+            {
+                sceneIsOn = false;
+                btnStartScene.Text = "Turn ON traffic lights";
+                TurnOFFCarGreen();
+                TurnOFFCarOrange();
+                TurnOFFCarRed();
+                TurnOFFPedestrianGreen();
+                TurnOFFPedestrianRed();
+                timerForAll.Stop();
+                timerStopage.Stop();
+                mode = "OrangeBlinkAllTheTime";
+
+                pressPedButton = false;
+                stateCounter = false;
+                timeCounter = 0;
+                stateCounter = false;
+                TrafficLightsChange();
+
+
+            }
+
+        }
+        private void btn_TurnONPedGreen_Click(object sender, EventArgs e)
+        {
+            if((timeCounter >= 10) && (mode == "CarGreenBlink"))
+            {
+                timerStopage.Stop();
+                timerForAll.Stop();
+                lb_PedWait.Text = "";
+                timeCounter = 0;
+                stateCounter = false;
+                mode = "CarGreenBlink";
+                TrafficLightsChange();         
+            }
+            else if(mode == "CarGreenBlink")
+            {
+                pressPedButton = true;
+            }
+            
+        }
+
+        private void timerStopage_Tick(object sender, EventArgs e)
+        {
+            if(mode == "CarGreenBlink")
+            {
+                timeCounter++;
+                lb_PedWait.Text = 20 - timeCounter  + "";
+            }
+            if(timeCounter >= 20)
+            {
+                timerStopage.Stop();
+                lb_PedWait.Text = "";
+            }
+            if(pressPedButton && timeCounter < 10)
+            {
+                lb_PedWait.Text = 10 - timeCounter + "";
+            }
+            if(timeCounter >= 10 && pressPedButton)
+            {
+                timerStopage.Stop();
+                timerForAll.Stop();
+                lb_PedWait.Text = "";
+                timeCounter = 0;
+                stateCounter = false;
+                mode = "CarGreenBlink";
+                TrafficLightsChange();
+            }
+            
+        }
+
+       
+    }
+}
